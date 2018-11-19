155d5b038a1f0e30c4d4b399ba7c60160fe80889
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2018-11-19 10:44:23 -0500

    Attempt to load active_record

--- lib/signalfx/tracing/instrumentation/active_record.rb
+++ lib/signalfx/tracing/instrumentation/active_record.rb
@@ -8,7 +8,13 @@ module SignalFx
         class << self
 
           def instrument(opts = {})
-            return if !defined?(::ActiveSupport::Notifications)
+            # load ActiveRecord and ActiveSupport if present
+            begin
+              require 'active_support'
+              require 'active_record'
+            rescue LoadError
+              return
+            end
 
             require 'active_record/opentracing'
             ::ActiveRecord::OpenTracing.instrument
