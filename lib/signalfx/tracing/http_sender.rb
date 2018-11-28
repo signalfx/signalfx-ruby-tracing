0eb69a87f6a6fa9a2a5faf21d0a03151903fad04
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2018-11-28 09:54:36 -0500

    Document need for HttpSenderWithFlags

--- lib/signalfx/tracing/http_sender.rb
+++ lib/signalfx/tracing/http_sender.rb
@@ -1,5 +1,13 @@
 require 'jaeger/client/http_sender'
 
+# This child class of HttpSender exists to allow Net::HTTP instrumentation to
+# ignore requests made by the tracer. The tracer uses the Thrift HTTP Transport,
+# which uses Net::HTTP internally, to send spans to the collector.
+#
+# The sender spins in a separate thread to send spans. The overridden
+# HttpSender#send_spans sets a thread variable, :http_sender_thread, which is
+# checked with a block passed to the Net::HTTP instrumentation.
+
 module SignalFx
   module Tracing
     class HttpSenderWithFlag < Jaeger::Client::HttpSender
