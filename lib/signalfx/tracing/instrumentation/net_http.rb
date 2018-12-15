af85712984390426b96e10493e64ea21f944665c
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2018-12-15 10:57:45 -0500

    Allow setting a custom tracer for Net::Http

--- lib/signalfx/tracing/instrumentation/net_http.rb
+++ lib/signalfx/tracing/instrumentation/net_http.rb
@@ -10,8 +10,9 @@ module SignalFx
           def instrument(opts = {})
             require 'net/http/instrumentation'
 
+            tracer = opts.fetch(:tracer, OpenTracing.global_tracer)
             ignore_request = lambda { Thread.current.thread_variable_get(:http_sender_thread) }
-            ::Net::Http::Instrumentation.instrument(ignore_request: ignore_request)
+            ::Net::Http::Instrumentation.instrument(tracer: tracer, ignore_request: ignore_request)
           end
         end
       end
