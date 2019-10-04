6aac1ada4a27beeaf3035ad3ee18dcef215761c6
Ryan Fitzpatrick <rmfitzpatrick@signalfx.com>
2019-10-04 18:54:49 -0400

    Bump mysql2 instrumentation version

--- gem.deps.rb
+++ gem.deps.rb
@@ -2,7 +2,7 @@ group :instrumentations do
   gem 'activerecord-opentracing', '~> 0.2.1'
   gem 'grape-instrumentation', "~> 0.1.0"
   gem 'mongodb-instrumentation', '~> 0.1.1'
-  gem 'mysql2-instrumentation', '~> 0.1.0'
+  gem 'mysql2-instrumentation', '~> 0.2.0'
   gem 'nethttp-instrumentation', '~> 0.1.2'
   gem 'rack-tracer', '~> 0.8'
   gem 'rails-instrumentation', '0.1.2'
