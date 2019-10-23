104feabce7971d2bbb1496ad93a298f15088797c
Ryan Fitzpatrick <rmfitzpatrick@signalfx.com>
2019-10-23 15:26:06 -0400

    Allow bootstrap dep installation from git

--- gem.deps.rb
+++ gem.deps.rb
@@ -2,10 +2,10 @@ group :instrumentations do
   gem 'activerecord-opentracing', '~> 0.2.1'
   gem 'grape-instrumentation', "~> 0.1.0"
   gem 'mongodb-instrumentation', '~> 0.1.1'
-  gem 'mysql2-instrumentation', '~> 0.2.0'
+  gem 'mysql2-instrumentation', '~> 0.2.1'
   gem 'nethttp-instrumentation', '~> 0.1.2'
-  gem 'rack-tracer', '~> 0.8'
-  gem 'rails-instrumentation', '0.1.2'
+  gem 'rack-tracer', git: 'git://github.com/signalfx/ruby-rack-tracer.git', branch: 'sfx_release'
+  gem 'rails-instrumentation', '0.1.3'
   gem 'redis-instrumentation', '~> 0.1.1'
   gem 'restclient-instrumentation', '~> 0.1.1'
   gem 'sequel-instrumentation', '~> 0.1.0'
